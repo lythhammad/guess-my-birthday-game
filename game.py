c066e8c00a79130a7444476a2ecc2f13cb2f1ea2
lyth hmmad <your@email.here>
2023-02-08 14:52:03 -0600

    Wrote a game

--- game.py
+++ game.py
@@ -1,76 +1,20 @@
 from random import randint
 
-name = input("Hi! whats your name!")
+name = input("Hi! whats your name?")
 
-guess_number = 1
-mounth_number = randint(1,12)
-day_number = randint(1, 31)
-year_number = randint(1995, 2008)
+for guess_number in range(1, 6):
+    mounth_number = randint(1,12)
+    day_number = randint(1, 31)
+    year_number = randint(1995, 2008)
 
-print("Guess 1 :", name, "were you born in ",mounth_number,"/", year_number,"/",day_number, "?")
+    print("Guess",guess_number, "were you born in ",mounth_number,"/", year_number,"/",day_number, "?")
 
-response = input("yes or no? ")
+    response = input("yes or no? ")
 
-if response == "yes":
-    print("i knew it")
-    exit()
-else:
-    print("drat! leme try again!")
-
-
-mounth_number = randint(1, 12)
-year_number = randint(1995, 2008)
-day_number = randint(1, 31)
-
-print("guess 2 :",name, "were you born in", mounth_number, "/", year_number,"/",day_number, "?")
-
-response = input("yes or no? ")
-
-if response == "yes":
-    print("i knew it!")
-    exit()
-else:
-    print("drat! lemme try again!")
-
-
-mounth_number = randint(1, 12)
-year_number = randint(1995, 2008)
-day_number = randint(1, 31)
-
-print("guess 3 :",name, "were you born in", mounth_number, "/", year_number,"/",day_number, "?")
-
-response = input("yes or no? ")
-
-if response == "yes":
-    print("i knew it!")
-    exit()
-else:
-    print("drat! lemme try again!")
-
-mounth_number = randint(1, 12)
-year_number = randint(1995, 2008)
-day_number = randint(1, 31)
-
-print("guess  :",name, "were you born in", mounth_number, "/", year_number,"/",day_number, "?")
-
-response = input("yes or no? ")
-
-if response == "yes":
-    print("i knew it!")
-    exit()
-else:
-    print("drat! lemme try again!")
-
-mounth_number = randint(1, 12)
-year_number = randint(1995, 2008)
-day_number = randint(1, 31)
-
-print("guess 5 :",name, "were you born in", mounth_number, "/", year_number,"/",day_number, "?")
-
-response = input("yes or no? ")
-
-if response == "yes":
-    print("took a min but i got it!")
-    exit()
-else:
-    print("I have batter thing to do in life bye!")
+    if response == "yes":
+        print("i knew it!")
+        exit()
+    elif guess_number == 5:
+        print("i have other things to do. good bye.")
+    else:
+        print("drat! leme try again!")
